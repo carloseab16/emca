193fce3b63e8598e9e2a0a63bdc480db74597293
verajulio823 <carloseab16@gmail.com>
2018-06-25 17:18:44 -0500

    first commits

---
+++ js/maps.js
@@ -0,0 +1,21 @@
+ 
+    function regular_map() {
+        var var_location = new google.maps.LatLng(-16.413097, -71.528707);;
+    
+        var var_mapoptions = {
+            center: var_location,
+            zoom: 18
+        };
+    
+        var var_map = new google.maps.Map(document.getElementById("map-container"),
+            var_mapoptions);
+    
+        var var_marker = new google.maps.Marker({
+            position: var_location,
+            map: var_map,
+            title: "New York"
+        });
+    }
+    
+    // Initialize maps
+    google.maps.event.addDomListener(window, 'load', regular_map);
